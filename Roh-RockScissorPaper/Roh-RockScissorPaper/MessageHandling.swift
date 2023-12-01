7a8c1ae75c6c1df1252d7048d9e9b1d57c4cd6e6
Hyung Woo Roh <67363759+rohhyungwoo@users.noreply.github.com>
2023-12-01 14:28:11 +0900

    remove ds store

--- Roh-RockScissorPaper/Roh-RockScissorPaper/MessageHandling.swift
+++ Roh-RockScissorPaper/Roh-RockScissorPaper/MessageHandling.swift
@@ -7,19 +7,8 @@
 
 
 class MessageHandling {
-    
-    let pc: String = "컴퓨터"
-    let user: String = "사용자"
-    
-    enum Message: String {
-        case menu = "가위(1), 바위(2), 보(3)! <종료 : 0> :"
-        case draw = "비겼습니다!"
-        case win = "이겼습니다!"
-        case lose = "졌습니다!"
-        case exit = "게임 종료"
-        case victory = "의 승리"
-    }
-    
+   
+
     func input() throws -> String {
         guard let pureInput = Swift.readLine() else { throw ErrorHandling.InvalidInputError }
         let refinedInput = pureInput.trimmingCharacters(in: .whitespacesAndNewlines)
